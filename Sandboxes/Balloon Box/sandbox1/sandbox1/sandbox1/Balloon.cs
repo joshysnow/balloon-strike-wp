9e492851569a3a43ada4b891e0186c4245fb6cfb
Joshua Hirst <hirst.joshua@me.com>
2014-02-05 21:31:03 +0000

    Master

---
+++ Sandboxes/Balloon Box/sandbox1/sandbox1/sandbox1/Balloon.cs
@@ -0,0 +1,77 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace sandbox1
+{
+    public enum BalloonColour : byte
+    {
+        Red = 0x01,
+        Green = 0x02,
+        Blue = 0x04
+    }
+
+    public class Balloon
+    {
+        private Texture2D _texture;
+
+        public BalloonColour Colour
+        {
+            get; set;
+        }
+
+        private Vector2 _position;
+        public Vector2 Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        private Vector2 _velocity;
+        public Vector2 Velocity
+        {
+            get
+            {
+                return _velocity;
+            }
+            set
+            {
+                _velocity = value;
+            }
+        }
+
+        public string Name
+        {
+            get; private set;
+        }
+
+        public Balloon(string name, ref Texture2D texture)
+        {
+            this.Name = name;
+            _texture = texture;
+            _position = new Vector2();
+        }
+
+        public void SetPosition(float x, float y)
+        {
+            _position.X = x;
+            _position.Y = y;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Position += Velocity;
+        }
+
+        public void Draw(ref SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, Color.White);
+        }
+    }
+}
---
+++ Sandboxes/Balloon Box/sandbox2/sandbox1/sandbox1/Sandbox.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
+
+namespace sandbox2
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Sandbox : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _greenTexture;
+        private Texture2D _redTexture;
+        private Texture2D _blueTexture;
+
+        private int _spawnCount;
+        private Vector2 _spawnVelocity;
+        private Random _randomPosition;
+
+        private LinkedList<Balloon> _balloonMemory;
+        private List<Balloon> _balloons;
+
+        private SpriteFont _debugFont;
+
+        private struct Timer
+        {
+            public float TimePassed;
+            public float TimeToSpawn;
+        }
+        private Timer _myTimer;
+
+        private int _screenWidth;
+        private int _screenHeight;
+
+        public Sandbox()
+        {
+            _screenHeight = 800;
+            _screenWidth = 480;
+
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferHeight = _screenHeight;
+            _graphics.PreferredBackBufferWidth = _screenWidth;
+            _graphics.ToggleFullScreen();
+            _graphics.SupportedOrientations = DisplayOrientation.Portrait;
+
+            Guide.IsScreenSaverEnabled = false;
+            base.IsFixedTimeStep = false;
+
+            this.Content.RootDirectory = "Content";
+
+            // Frame rate is 30 fps by default for Windows Phone.
+            this.TargetElapsedTime = TimeSpan.FromTicks(333333);
+
+            // Extend battery life under lock.
+            this.InactiveSleepTime = TimeSpan.FromSeconds(1);
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            _myTimer = new Timer() { TimePassed = 0f, TimeToSpawn = 25000f };
+
+            _spawnCount = 0;
+            _spawnVelocity = new Vector2(0, -5.1f);
+            _randomPosition = new Random(DateTime.Now.Millisecond);
+
+            _balloonMemory = new LinkedList<Balloon>();
+            _balloons = new List<Balloon>();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _redTexture = this.Content.Load<Texture2D>("Balloons/red200");
+            _blueTexture = this.Content.Load<Texture2D>("Balloons/blue200");
+            _greenTexture = this.Content.Load<Texture2D>("Balloons/green200");
+            _debugFont = this.Content.Load<SpriteFont>("Text");
+
+            // Populate pool.
+            Balloon b;
+            while (_spawnCount < 7)
+            {
+                b = new Balloon("Balloon" + _spawnCount++, ref _redTexture);
+                b.Velocity = _spawnVelocity;
+                _balloonMemory.AddFirst(b);
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                this.Exit();
+            }
+
+            int index = 0;
+            Balloon b;
+            while (index < _balloons.Count)
+            {
+                b = _balloons[index];
+                if (b.Position.Y < 0)
+                {
+                    _balloonMemory.AddFirst(b);
+                    _balloons.RemoveAt(index);
+                }
+                else
+                {
+                    b.Update(gameTime);
+                    index++;
+                }
+            }
+
+            _myTimer.TimePassed += gameTime.TotalGameTime.Milliseconds;
+
+            if (_myTimer.TimePassed >= _myTimer.TimeToSpawn)
+            {
+                _myTimer.TimePassed %= _myTimer.TimeToSpawn;
+                TimerElapsed();
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            int index = 0;
+            while (index < _balloons.Count)
+            {
+                _balloons[index].Draw(ref _spriteBatch);
+                index++;
+            }
+
+            _spriteBatch.DrawString(_debugFont, "Memory Pool: " + _balloonMemory.Count, Vector2.Zero, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void TimerElapsed()
+        {
+            Balloon spawn;
+
+            if (_balloonMemory.Count > 0)
+            {
+                spawn = _balloonMemory.First.Value;
+                _balloonMemory.RemoveFirst();
+            }
+            else
+            {
+                spawn = new Balloon("Balloon" + _spawnCount, ref _redTexture);
+                spawn.Colour = BalloonColour.Red;
+                spawn.Velocity = _spawnVelocity;
+                _spawnCount++;
+            }
+
+            // Next position from 0 to the visible edge of the texture.
+            int x = _randomPosition.Next(_screenWidth - _redTexture.Width);
+            spawn.SetPosition(x,_screenHeight);
+
+            _balloons.Add(spawn);
+        }
+    }
+}
