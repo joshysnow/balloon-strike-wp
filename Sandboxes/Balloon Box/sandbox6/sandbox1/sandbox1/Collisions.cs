b175a52b443828ebf3ceabe567fc863e827f948c
Joshua Hirst <hirst.joshua@me.com>
2014-02-16 12:00:35 +0000

    Sandbox 6 start.

---
+++ Sandboxes/Balloon Box/sandbox6/sandbox1/sandbox1/Collisions.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace sandbox6
+{
+    public static class Collisions
+    {
+        public static bool Intersects(Vector2 upperLeft, Vector2 lowerRight, Vector2 position)
+        {
+            return (position.X >= upperLeft.X) && (position.X <= lowerRight.X) &&
+                (position.Y >= upperLeft.Y) && (position.Y <= lowerRight.Y);
+        }
+    }
+}
---
+++ Sandboxes/Balloon Box/sandbox6/sandbox1/sandbox1/MonoSpawnTimer.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace sandbox6
+{
+    public class MonoSpawnTimer
+    {
+        private float _timePassed;
+        private float _timeToSpawn;
+        private bool _initialized;
+
+        public MonoSpawnTimer()
+        {
+            _initialized = false;
+        }
+
+        /// <summary>
+        /// Initialize a single repeating timer.
+        /// </summary>
+        /// <param name="spawnTime"></param>
+        public void Initialize(float spawnTime)
+        {
+            _timePassed = 0f;
+            _timeToSpawn = spawnTime;
+            _initialized = true;
+        }
+
+        public bool Update(GameTime gameTime) 
+        {
+            if (!_initialized)
+            {
+                return false;
+            }
+
+            _timePassed += gameTime.ElapsedGameTime.Milliseconds;
+
+            if (_timePassed >= _timeToSpawn)
+            {
+                _timePassed %= _timeToSpawn;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
