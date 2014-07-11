5d7bd4c3259f8be4deef301879da07a359b39373
Joshua Hirst <hirst.joshua@me.com>
2014-07-11 21:11:40 +0100

    R1. Main menu loop finished.

--- Releases/Balloon Strike/BalloonStrike/BalloonStrike/Views/AboutView.cs
+++ Releases/Balloon Strike/BalloonStrike/BalloonStrike/Views/AboutView.cs
@@ -2,6 +2,7 @@ using System;
 using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
 using GameFramework;
 using GameInterfaceFramework;
 
@@ -12,14 +13,24 @@ namespace BalloonStrike.Views
         private SpriteFont _font;
         private string _message;
 
+        public AboutView()
+        {
+            _transitionOnTime = TimeSpan.FromSeconds(0.5);
+            _transitionOffTime = TimeSpan.FromSeconds(0.5);
+
+            _viewGestures = GestureType.Tap;
+        }
+
         public override void Activate(bool instancePreserved)
         {
             if (!instancePreserved)
             {
                 _font = ResourceManager.Manager.GetFont("debug");
-                _message = Assembly.GetExecutingAssembly().FullName;
-                //Assembly.GetExecutingAssembly();
-                //_message = "Balloon Strike\nVersion: " + Assembly.GetExecutingAssembly().GetName().Version + "\nFoxCode Studios\nSupport: example@email.com";
+
+                const byte NAME = 0;
+                const byte VERSION = 1;
+                string[] info = Assembly.GetExecutingAssembly().FullName.Split(',');
+                _message = info[NAME].Trim() + "\n" + info[VERSION].Trim();
             }
         }
 
@@ -36,7 +47,7 @@ namespace BalloonStrike.Views
         {
             SpriteBatch spriteBatch = ViewManager.SpriteBatch;
             spriteBatch.Begin();
-            spriteBatch.DrawString(_font, _message, Vector2.Zero, Color.SpringGreen);
+            spriteBatch.DrawString(_font, _message, Vector2.Zero, Color.SpringGreen * TransitionAlpha);
             spriteBatch.End();
         }
     }
--- Releases/Balloon Strike/BalloonStrike/BalloonStrike/Views/GameOverView.cs
+++ Releases/Balloon Strike/BalloonStrike/BalloonStrike/Views/GameOverView.cs
@@ -2,14 +2,13 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input.Touch;
-using GameFramework;
 using GameInterfaceFramework;
 
 namespace BalloonStrike.Views
 {
-    public class GameView : View
+    public class GameOverView : View
     {
-        public GameView()
+        public GameOverView()
         {
             _transitionOnTime = TimeSpan.FromSeconds(0.5);
             _transitionOffTime = TimeSpan.FromSeconds(0.5);
@@ -19,24 +18,16 @@ namespace BalloonStrike.Views
 
         public override void HandlePlayerInput(ControlsState controls)
         {
-            if (controls.BackButtonPressed())
+            if (controls.BackButtonPressed() || controls.Gestures.Length > 0)
             {
                 Exit();
                 LoadView.Load(ViewManager, 1, new MainMenuView());
-            }
-            else
-            {
-                if (controls.Gestures.Length > 0)
-                {
-                    Exit();
-                    LoadView.Load(ViewManager, 1, new GameOverView());
-                }
             }
         }
 
         public override void Draw(GameTime gameTime)
         {
-            ViewManager.GraphicsDevice.Clear(Color.Violet * TransitionAlpha);
+            ViewManager.GraphicsDevice.Clear(Color.Red * TransitionAlpha);
         }
     }
 }
