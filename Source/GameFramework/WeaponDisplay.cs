299dbde13fe11929f76614956a103037f8d7d3dc
Joshua Hirst <hirst.joshua@icloud.com>
2015-07-22 08:48:50 +0100

    R1. [1] WeaponDisplay changing name before reappearing again when picking up a new weapon, fixed.

--- Source/GameFramework/WeaponDisplay.cs
+++ Source/GameFramework/WeaponDisplay.cs
@@ -8,7 +8,8 @@ namespace GameFramework
     public class WeaponDisplay
     {
         private Transition _transition;
-        private string _weaponName;
+        private string _currentName;
+        private string _nextName;
 
         public WeaponDisplay(string initialName)
         {
@@ -17,13 +18,13 @@ namespace GameFramework
             _transition.TransitionOffTime = TimeSpan.FromSeconds(1.5);
             _transition.Invoked = true; // Wait for a new weapon before transitioning off.
 
-            _weaponName = initialName;
+            _currentName = initialName;
         }
 
         public void WeaponChange(string newWeaponName)
         {
             _transition.State = TransitionState.TransitionOff;
-            _weaponName = newWeaponName;
+            _nextName = newWeaponName;
         }
 
         public void Update(GameTime gameTime)
@@ -31,18 +32,21 @@ namespace GameFramework
             _transition.Update(gameTime);
 
             if (_transition.State == TransitionState.Hidden)
+            {
+                _currentName = _nextName;
                 _transition.State = TransitionState.TransitionOn;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             SpriteFont font = ResourceManager.Resources.GetFont("debug");
-            Vector2 size = font.MeasureString(_weaponName);
+            Vector2 size = font.MeasureString(_currentName);
             Vector2 position = new Vector2() { 
                 X = (240 - (size.X / 2)), 
                 Y = ((800 - size.Y) - 10) };            
 
-            spriteBatch.DrawString(font, _weaponName, position, Color.Black * _transition.TransitionAlpha);
+            spriteBatch.DrawString(font, _currentName, position, Color.Black * _transition.TransitionAlpha);
         }
     }
 }
