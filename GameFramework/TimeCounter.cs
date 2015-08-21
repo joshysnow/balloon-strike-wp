f6ab8931088d1e6fad3ba326d3dc67d52f7e7126
Joshua Hirst <hirst.joshua@me.com>
2015-08-21 23:38:00 +0100

    R1. [1] Created a TimeCounter object to be delegated to the spawner.

---
+++ GameFramework/TimeCounter.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace GameFramework
+{
+    /// <summary>
+    /// This class is to be used to simply keep track of time.
+    /// No thrills or spills, it can be used as a delegate for more complex
+    /// objects like Timers, Triggers, Spawners etc.
+    /// </summary>
+    public class TimeCounter
+    {
+        public bool Elapsed
+        {
+            get { return _elapsed; }
+        }
+
+        public float Count
+        {
+            get { return _count; }
+        }
+
+        private float _count;
+        private float _limit;
+        private bool _elapsed;
+
+        public TimeCounter(TimeSpan limit)
+        {
+            _count = 0;
+            _limit = (float)limit.TotalMilliseconds;
+            _elapsed = false;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (_elapsed == false)
+            {
+                _count += gameTime.ElapsedGameTime.Milliseconds;
+
+                if (_count >= _limit)
+                {
+                    _elapsed = true;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+            _elapsed = false;
+        }
+    }
+}
