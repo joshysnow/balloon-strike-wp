62e94a0e3ef304b21766085a9dfebe4e68b7f34c
Joshua Hirst <hirst.joshua@me.com>
2015-09-07 19:26:43 +0100

    R1. [1] Refactored GameOverView.

--- BalloonStrike/BalloonStrike/Views/GameOverView.cs
+++ BalloonStrike/BalloonStrike/Views/GameOverView.cs
@@ -32,61 +32,9 @@ namespace BalloonStrike.Views
         {
             if (!instancePreserved)
             {
-                GraphicsDevice graphics = ViewManager.GraphicsDevice;
-                int screenWidth = graphics.Viewport.Width;
-                int screenHeight = graphics.Viewport.Height;
-
-                ResourceManager resources = ResourceManager.Resources;
-                _score = 0;
-                _scoreFont = resources.GetFont("score");
-
-#warning TODO: Calculate the central position for the final score using the score manager. 
-#warning This is so the score won't move from right to left as the digits increase from tens, to hundreds etc. (regarding positioning)
-
-                _titleFont = ResourceManager.Resources.GetFont("your_score");
-                Vector2 titleSize = _titleFont.MeasureString(YOUR_SCORE);
-                _titlePosition = new Vector2((screenWidth - titleSize.X) / 2, (screenHeight / 2) - titleSize.Y);
-
-                Player p = Player.Instance;
-                Vector2 scoreSize = _scoreFont.MeasureString(p.CurrentScore.ToString());
-                _scorePosition = new Vector2((screenWidth - scoreSize.X) / 2, (screenHeight / 2));
-
-                // If the player scored 0, then we have already finished counting.
-                _finishedCounting = (p.CurrentScore == 0);
-
-                // Ensure that if the score would take longer than usual then limit to 1/2 a second. In other words,
-                // if the score would take more than 1/2 a second to increment by one, then calculate the above to ensure
-                // it takes no longer than 1/2 a second.
-
-                // Calculate the amount to increment for large scores.
-                if (_finishedCounting == false)
-                    _increment = (33.3f / 500) * p.CurrentScore; // Finish in 1/2 a second. (FPS / time to finish) * score.
-                else
-                    EnableViewGestures();
-
-                Texture2D playTexture = resources.GetTexture("button_playagain");
-                Texture2D mainMenuTexture = resources.GetTexture("button_mainmenu");
-
-                const int BUTTON_HORIZONTAL_SPACING = 10;
-
-                int y = (screenHeight - (screenHeight / 4)) - (playTexture.Height / 2);
-                int x = (screenWidth / 2) - BUTTON_HORIZONTAL_SPACING - playTexture.Width;
-
-                Button playAgain = new Button(playTexture) { 
-                    Position = new Vector2(x, y) 
-                };
-
-                playAgain.Tapped += PlayTappedHandler;
-                _menuButtons.Add(playAgain);
-
-                x = (screenWidth / 2) + BUTTON_HORIZONTAL_SPACING;
-
-                Button mainMenu = new Button(mainMenuTexture) { 
-                    Position = new Vector2(x, y) 
-                };
-
-                mainMenu.Tapped += MenuTappedHandler;
-                _menuButtons.Add(mainMenu);
+                LoadResources();
+                SetupButtons();
+                SetupScore();
             }
         }
 
@@ -95,7 +43,8 @@ namespace BalloonStrike.Views
             if (State == TransitionState.Active && !_finishedCounting)
             {
                 Player player = Player.Instance;
-                if (_score < player.CurrentScore)
+
+                if (_score <= player.CurrentScore)
                 {
                     if ((_score + _increment) >= player.CurrentScore)
                     {
@@ -124,6 +73,73 @@ namespace BalloonStrike.Views
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void LoadResources()
+        {
+            ResourceManager resources = ResourceManager.Resources;
+            _scoreFont = resources.GetFont("score");
+            _titleFont = resources.GetFont("your_score");
+        }
+
+        private void SetupButtons()
+        {
+            ResourceManager resources = ResourceManager.Resources;
+
+            GraphicsDevice graphics = ViewManager.GraphicsDevice;
+            int screenWidth = graphics.Viewport.Width;
+            int screenHeight = graphics.Viewport.Height;
+
+            Texture2D playTexture = resources.GetTexture("button_playagain");
+            Texture2D mainMenuTexture = resources.GetTexture("button_mainmenu");
+
+            const int BUTTON_HORIZONTAL_SPACING = 10;
+
+            int y = (screenHeight - (screenHeight / 4)) - (playTexture.Height / 2);
+            int x = (screenWidth / 2) - BUTTON_HORIZONTAL_SPACING - playTexture.Width;
+
+            Button playAgain = new Button(playTexture)
+            {
+                Position = new Vector2(x, y)
+            };
+
+            x = (screenWidth / 2) + BUTTON_HORIZONTAL_SPACING;
+
+            Button mainMenu = new Button(mainMenuTexture)
+            {
+                Position = new Vector2(x, y)
+            };
+
+            playAgain.Tapped += PlayTappedHandler;
+            mainMenu.Tapped += MenuTappedHandler;
+
+            _menuButtons.Add(playAgain);  
+            _menuButtons.Add(mainMenu);
+        }
+
+        public void SetupScore()
+        {
+            GraphicsDevice graphics = ViewManager.GraphicsDevice;
+            int screenWidth = graphics.Viewport.Width;
+            int screenHeight = graphics.Viewport.Height;
+
+            int playerScore = Player.Instance.CurrentScore;
+
+            Vector2 titleSize = _titleFont.MeasureString(YOUR_SCORE);
+            _titlePosition = new Vector2((screenWidth - titleSize.X) / 2, (screenHeight / 2) - titleSize.Y);
+
+            Vector2 scoreSize = _scoreFont.MeasureString(playerScore.ToString());
+            _scorePosition = new Vector2((screenWidth - scoreSize.X) / 2, (screenHeight / 2));
+
+            // TODO: Calculate the central position for the final score using the score manager.
+            // This is so the score won't move from right to left as the digits increase from tens, to hundreds etc. (regarding positioning)
+
+            // Ensure that if the score would take longer than usual then limit to 1/2 a second. In other words,
+            // if the score would take more than 1/2 a second to increment by one, then calculate the above to ensure
+            // it takes no longer than 1/2 a second.
+
+            // Calculate the amount to increment for large scores.
+            _increment = (33.3f / 500) * playerScore; // Finish in 1/2 a second. (FPS / time to finish) * score.
         }
 
         protected override void HandleBackButtonPressed()
